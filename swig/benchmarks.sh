be1c9ac8d58c7d8762c8b579de9ac62fa0ec014e
Amit Patankar <amitpatankar@google.com>
2019-03-20 17:00:36 -0700

    Updatnig SWIG basic benchmarks for C++ to Python.

--- swig/benchmarks.sh
+++ swig/benchmarks.sh
@@ -2,11 +2,11 @@
 
 pushd swig
 
-virtualenv venv
+virtualenv -p python3.5 venv
 source ${PWD}/venv/bin/activate
 
 swig -c++ -python swig_example.i
-g++ -fpic -c swig_example.h swig_example_wrap.cxx swig_example.cpp -I${PWD}/venv/include/python2.7/
+g++ -fpic -c swig_example.h swig_example_wrap.cxx swig_example.cpp -I${PWD}/venv/include/python3.5m/
 gcc -shared swig_example_wrap.o swig_example.o -o _swig_example.so -lstdc++
 
 python benchmarks.py
